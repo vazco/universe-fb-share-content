677c099fd4a94af6165b323d3c46b10739f98a0f
cristo-rabani <cristo.rabani@gmail.com>
2016-06-29 15:03:30 +0200

    up

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
     name: 'universe:fb-share-content',
     version: '0.1.0',
     // Brief, one-line summary of the package.
-    summary: '',
+    summary: 'Publish post to timeline (works for user and facebook pages)',
     // URL to the Git repository containing the source code for this package.
     git: 'https://github.com/vazco/universe-fb-share-content',
     // By default, Meteor will default to using README.md for documentation.
